04dabd7d424489e49a73c6714ca66c658ceae3b7
berinbpc <berinibruno00@gmail.com>
2026-03-27 16:12:28 -0300

    cambios en db.py: agregado sqlite si no hay server

--- src/config/db.py
+++ src/config/db.py
@@ -5,9 +5,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 MYSQL_SERVER = os.getenv("MYSQL_SERVER")
-MYSQL_USER = os.getenv("MYSQL_USER")
-MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
-MYSQL_DB = os.getenv("MYSQL_DB")
-MYSQL_PORT = os.getenv("MYSQL_PORT")
-url = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_SERVER}:{MYSQL_PORT}/{MYSQL_DB}"
+
+if not MYSQL_SERVER:
+    url = "sqlite:///./database.db"
+else:
+
+    MYSQL_USER = os.getenv("MYSQL_USER", "root")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD","")
+    MYSQL_DB = os.getenv("MYSQL_DB", "logitrack_db")
+    MYSQL_PORT = os.getenv("MYSQL_PORT", "3306")
+    url = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_SERVER}:{MYSQL_PORT}/{MYSQL_DB}"
+    
 engine = create_engine(url)
