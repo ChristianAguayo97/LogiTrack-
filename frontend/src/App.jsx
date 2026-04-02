f34e342e3bee795d421bc99a4a5a8f720e1ee557
MaxiAlegre18 <maxialegre2003@gmail.com>
2026-04-02 14:01:49 -0300

    feat: agregados mas campos en el listado de envios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -33,19 +33,25 @@ const App = () => {
       <table className='tabla-envios'>
         <thead>
           <tr>
-            <th>ID envio</th>
-            <th>Destinatario</th>
+            <th>Tracking ID</th>
+            <th>Fecha de alta</th>
+            <th>Remitente ID</th>
+            <th>Destinatario ID</th>
             <th>Peso (kg)</th>
             <th>Estado</th>
+            <th>Prioridad</th>
           </tr>
         </thead>
         <tbody>
           {envios.map(envio => (
             <tr key={envio.tracking_id} className='fila-envio'>
               <td>{envio.tracking_id}</td>
+              <td>{envio.f_creacion}</td>
+              <td>{envio.remitente_id}</td>
               <td>{envio.destinatario_id}</td>
               <td>{envio.peso_paquete}</td>
               <td>{envio.estado}</td>
+              <td>{envio.prioridad}</td>
             </tr>
           ))} 
         </tbody>
